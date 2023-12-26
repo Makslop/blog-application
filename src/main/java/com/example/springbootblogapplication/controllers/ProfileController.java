29ee082115c2487c96a90157d79552f9ba93275a
Nurkhan <31109@iitu.edu.kz>
2023-12-26 13:22:00 +0600

    Profile edit page work

--- src/main/java/com/example/springbootblogapplication/controllers/ProfileController.java
+++ src/main/java/com/example/springbootblogapplication/controllers/ProfileController.java
@@ -7,11 +7,16 @@ import com.example.springbootblogapplication.services.FileService;
 import com.example.springbootblogapplication.services.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,6 +41,59 @@ public class ProfileController {
         } else {
             return "404";
         }
+    }
+
+    @GetMapping("/profile/{id}/edit")
+    @PreAuthorize("isAuthenticated()")
+    public String getProfileForEdit(@PathVariable Long id, Model model, Principal principal) {
+
+        String authUsername = "anonymousUser";
+        if (principal != null) {
+            authUsername = principal.getName();
+        }
+
+        // find post by id
+        Optional<Account> optionalProfile = accountService.getById(id);
+
+        // if post exist and belongs to the authenticated user, put it in model
+        if (optionalProfile.isPresent()) {
+            Account profile = optionalProfile.get();
+
+            // Check if the post belongs to the authenticated user
+            if (profile.getEmail().equals(authUsername)) {
+                model.addAttribute("profile", profile);
+                return "profile_edit";
+            } else {
+                return "403";
+            }
+        } else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/profile/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String updateProfile(@PathVariable Long id, Account profile, @RequestParam("file") MultipartFile file) {
+
+        Optional<Account> optionalProfile = accountService.getById(id);
+        if (optionalProfile.isPresent()) {
+            Account existingProfile = optionalProfile.get();
+
+            existingProfile.setFirstName(profile.getFirstName());
+            existingProfile.setLastName(profile.getLastName());
+            existingProfile.setGender(profile.getGender());
+            existingProfile.setAboutMe(profile.getAboutMe());
+            try {
+                fileService.save(file);
+                existingProfile.setAvatarFilePath(file.getOriginalFilename());
+            } catch (Exception e) {
+                log.error("Error processing file: {}", file.getOriginalFilename());
+            }
+
+            accountService.save(existingProfile);
+        }
+
+        return "redirect:/profile/" + profile.getId();
     }
 
 
